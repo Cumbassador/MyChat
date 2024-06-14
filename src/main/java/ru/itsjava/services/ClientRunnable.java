273b9898a9e8d4d3f4e31f3eddba7789c146e444
Cumbassador <163416241+Cumbassador@users.noreply.github.com>
2024-06-14 07:30:32 +0300

    version1.2

--- src/main/java/ru/itsjava/services/ClientRunnable.java
+++ src/main/java/ru/itsjava/services/ClientRunnable.java
@@ -10,14 +10,17 @@ import java.net.Socket;
 @RequiredArgsConstructor
 public class ClientRunnable implements Runnable,Observer{
     private final Socket socket;
+    private final ServerService serverService;
     @SneakyThrows
     @Override
     public void run(){
         System.out.println("Client connected");
+        serverService.addObserver(this);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String messegeFromClient;
         while((messegeFromClient=bufferedReader.readLine())!=null){
             System.out.println( messegeFromClient);
+            serverService.notifyObservers(messegeFromClient);
         }
         System.out.println(bufferedReader.readLine());
 
